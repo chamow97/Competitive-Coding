06f199a65a488e09337c460e7c2abf446b6174bc
Chandramowli J <chamowstudios@outlook.com>
2017-01-01 19:53:06 +0530

    Modified Two buttons

--- Codeforces/two buttons.cpp
+++ Codeforces/two buttons.cpp
@@ -1,38 +1,62 @@
+//template by chamow
+
 #include<bits/stdc++.h>
 using namespace std;
+#define rep(i,val,n) for(int i=val;i<n;i++)
+#define per(j,val,n) for(int j=val;j>=n;j--)
 #define pb push_back
+#define pi 3.14157
+typedef long long ll;
+ll d[30011],n,m;
+ll readInt () {
+	bool minus = false;
+	ll result = 0;
+	char ch;
+	ch = getchar();
+	while (true) {
+		if (ch == '-') break;
+		if (ch >= '0' && ch <= '9') break;
+		ch = getchar();
+	}
+	if (ch == '-') minus = true; else result = ch-'0';
+	while (true) {
+		ch = getchar();
+		if (ch < '0' || ch > '9') break;
+		result = result*10 + (ch - '0');
+	}
+	if (minus)
+		return -result;
+	else
+		return result;
+}
+
+void dfs(ll s, ll t)
+{
+	if(s<=0 || s>=m*2)
+	{
+		return;
+	}
+	if(d[s]<=t)
+	{
+		return;
+	}
+	d[s] = t;
+	dfs(s*2, t+1);
+	dfs(s-1, t+1);
+}
 int main()
 {
-    int j,i,d[10001];
-    bool visited[10001];
-    vector<int> *vec;
-    int n,m,k,u,v;
-    cin>>n>>m;
-    vec = new vector<int>[10001];
-    for(i=1;i<=m*2;i++)
-    {
-        d[i] = INT_MAX;
-        visited[i] = false;
-    }
-    vec[1].pb(2);
-    for(i=2;i<=m*2;i++)
-    {
-            vec[i].pb(i-1);
-            vec[i].pb(i*2);
-    }
-   d[n] = 0;
-    visited[n] = true;
-    for(i=n;i<=m;i++)
-    {
-        vector<int>::iterator it;
-        for(it=vec[i].begin();it!=vec[i].end();it++)
-        {
-            if(visited[*it]==false && d[*it] > (1+d[i]))
-            {
-                d[*it] = 1+d[i];
-            }
-        }
-    }
-    cout<<d[m];
+	n = readInt();
+	m = readInt();
+	memset(d,1,sizeof(d));
+	if(n>=m)
+	{
+		cout<<n-m;
+	}
+	else
+	{
+		dfs(n,0);
+		cout<<d[m];	
+	}		
+    return 0;
 }
- 
