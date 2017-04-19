72a279cf891739a3ea80b5ef4d9998e4c2d38cf0
Chandramowli J <chamowstudios@outlook.com>
2017-04-19 21:17:19 +0530

    Add problems

---
+++ codeforces/cheap travel.cpp
@@ -0,0 +1,79 @@
+
+//template by chamow
+#include<bits/stdc++.h>
+/*-------------------------------------------------------- */
+using namespace std;
+
+/*-------------------------------------------------------- */
+
+#define rep(i,val,n) for(ll i=val;i<n;i++)
+#define per(j,val,n) for(ll j=val;j>=n;j--)
+#define pb push_back
+#define pi 3.14157
+#define mp make_pair
+
+/*-------------------------------------------------------- */
+
+typedef long long ll;
+typedef vector<bool> boolean;
+typedef vector<ll> vec;
+
+
+/*-------------------------------------------------------- */
+
+ll gcd(ll a, ll b)
+{
+	if(b == 0)
+	{
+		return a;
+	}
+	return gcd(b, a%b);
+}
+
+ll lcm(ll a, ll b)
+{
+	return ((a*b)/gcd(a,b));
+}
+
+long long int read_int(){
+	char r;
+	bool start=false,neg=false;
+	long long int ret=0;
+	while(true){
+		r=getchar();
+		if((r-'0'<0 || r-'0'>9) && r!='-' && !start){
+			continue;
+		}
+		if((r-'0'<0 || r-'0'>9) && r!='-' && start){
+			break;
+		}
+		if(start)ret*=10;
+		start=true;
+		if(r=='-')neg=true;
+		else ret+=r-'0';
+	}
+	if(!neg)
+		return ret;
+	else
+		return -ret;
+}
+/*-------------------------------------------------------- */
+
+int main()
+{
+    ll n, m, a, b, ans = 0;
+    n = read_int();
+    m = read_int();
+    a = read_int();
+    b = read_int();
+    if(m >= n)
+    {
+        cout<<min(a*n,b);
+        return 0;
+    }
+    ans = min(((n/m)*b) + ((n%m)*a), n*a);
+    ans = min(ans, (n%m) == 0 ? (n/m)*b : ((n/m)+1)*b);
+    cout<<ans;
+
+	return 0;
+}
