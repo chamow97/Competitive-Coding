6ebab617c013dc7fee7a28fc41d2cdb48e740064
Chandramowli J <chamowstudios@outlook.com>
2017-04-26 20:45:18 +0530

    Add pythagorean triplet problem

---
+++ codeforces/pythagorean triplet.cpp
@@ -0,0 +1,85 @@
+//template by chamow
+#include<bits/stdc++.h>
+/*-------------------------------------------------------- */
+using namespace std;
+
+/*-------------------------------------------------------- */
+
+#define rep(i,val,n) for(ll i=val;i<n;i++)
+#define per(j,val,n) for(ll j=val;j>=n;j--)
+#define pb push_back
+#define pi 3.14157
+#define mp make_pair
+#define MODULO 1000000007
+#define INF 1000000000000000
+
+/*-------------------------------------------------------- */
+
+typedef long long ll;
+typedef vector<bool> boolean;
+typedef vector<ll> vec;
+
+
+/*-------------------------------------------------------- */
+
+ll gcd(ll a, ll b)
+{
+	if(b == 0)
+	{
+		return a;
+	}
+	return gcd(b, a%b);
+}
+
+ll lcm(ll a, ll b)
+{
+	return ((a*b)/gcd(a,b));
+}
+
+long long int read_int(){
+	char r;
+	bool start=false,neg=false;
+	long long int ret=0;
+	while(true){
+		r=getchar();
+		if((r-'0'<0 || r-'0'>9) && r!='-' && !start){
+			continue;
+		}
+		if((r-'0'<0 || r-'0'>9) && r!='-' && start){
+			break;
+		}
+		if(start)ret*=10;
+		start=true;
+		if(r=='-')neg=true;
+		else ret+=r-'0';
+	}
+	if(!neg)
+		return ret;
+	else
+		return -ret;
+}
+/*-------------------------------------------------------- */
+
+int main()
+{
+    ll n, m, k;
+    n = read_int();
+    if(n == 1 || n == 2)
+    {
+        cout<<-1;
+        return 0;
+    }
+    if((n&1))
+    {
+        m = ((pow(n,2))-1)/2;
+        k = ((pow(n,2))+1)/2;
+    }
+    else
+    {
+        m = ((pow(n,2))/4)-1;
+        k = ((pow(n,2))/4)+1;
+    }
+    cout<<m<<" "<<k;
+
+	return 0;
+}
