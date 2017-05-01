6e040fc6cd22ff299626f1ffb906d29311eab9b4
chamow <chamowstudios@outlook.com>
2017-05-01 23:49:26 +0530

    Add today's problems

---
+++ codeforces/football.cpp
@@ -0,0 +1,90 @@
+//template by chamow
+#include<bits/stdc++.h>
+/*-------------------------------------------------------- */
+using namespace std;
+
+/*-------------------------------------------------------- */
+
+#define rep(i,val,n) for(ll i=val;i<n;i++)
+#define per(j,val,n) for(ll j=val;j>=n;j--)
+#define pb push_back
+#define pi 3.14157
+#define mp make_pair
+#define MODULO 1000000007
+#define INF 1000000000000000
+
+/*-------------------------------------------------------- */
+
+typedef long long ll;
+typedef vector<bool> boolean;
+typedef vector<ll> vec;
+
+
+/*-------------------------------------------------------- */
+
+ll gcd(ll a, ll b)
+{
+	if(b == 0)
+	{
+		return a;
+	}
+	return gcd(b, a%b);
+}
+
+ll lcm(ll a, ll b)
+{
+	return ((a*b)/gcd(a,b));
+}
+
+long long int read_int(){
+	char r;
+	bool start=false,neg=false;
+	long long int ret=0;
+	while(true){
+		r=getchar();
+		if((r-'0'<0 || r-'0'>9) && r!='-' && !start){
+			continue;
+		}
+		if((r-'0'<0 || r-'0'>9) && r!='-' && start){
+			break;
+		}
+		if(start)ret*=10;
+		start=true;
+		if(r=='-')neg=true;
+		else ret+=r-'0';
+	}
+	if(!neg)
+		return ret;
+	else
+		return -ret;
+}
+/*-------------------------------------------------------- */
+
+int main()
+{
+	string s;
+	cin>>s;
+	vec dp(101,0);
+	ll maximum = 0;
+	rep(i,1,s.length())
+	{
+		if(s[i] == s[i-1])
+		{
+			dp[i] = 1 + dp[i-1];
+		}
+		else
+		{
+			dp[i] = 0;
+		}
+		maximum = max(maximum, dp[i]);
+	}
+	if(maximum >= 6)
+	{
+		cout<<"YES";
+	}
+	else
+	{
+		cout<<"NO";
+	}
+	return 0;
+}
---
+++ codeforces/table decorations.cpp
@@ -0,0 +1,80 @@
+//template by chamow
+#include<bits/stdc++.h>
+/*-------------------------------------------------------- */
+using namespace std;
+
+/*-------------------------------------------------------- */
+
+#define rep(i,val,n) for(ll i=val;i<n;i++)
+#define per(j,val,n) for(ll j=val;j>=n;j--)
+#define pb push_back
+#define pi 3.14157
+#define mp make_pair
+#define MODULO 1000000007
+#define INF 1000000000000000
+
+/*-------------------------------------------------------- */
+
+typedef long long ll;
+typedef vector<bool> boolean;
+typedef vector<ll> vec;
+
+
+/*-------------------------------------------------------- */
+
+ll gcd(ll a, ll b)
+{
+	if(b == 0)
+	{
+		return a;
+	}
+	return gcd(b, a%b);
+}
+
+ll lcm(ll a, ll b)
+{
+	return ((a*b)/gcd(a,b));
+}
+
+long long int read_int(){
+	char r;
+	bool start=false,neg=false;
+	long long int ret=0;
+	while(true){
+		r=getchar();
+		if((r-'0'<0 || r-'0'>9) && r!='-' && !start){
+			continue;
+		}
+		if((r-'0'<0 || r-'0'>9) && r!='-' && start){
+			break;
+		}
+		if(start)ret*=10;
+		start=true;
+		if(r=='-')neg=true;
+		else ret+=r-'0';
+	}
+	if(!neg)
+		return ret;
+	else
+		return -ret;
+}
+/*-------------------------------------------------------- */
+
+int main()
+{
+	ll a[3], d, e, f;
+	ll ans = 0;
+	a[0] = read_int();
+	a[1] = read_int();
+	a[2] = read_int();
+	sort(a,a+3);
+	if(a[2] >= (2*(a[0] + a[1])))
+	{
+		cout<<a[0] + a[1];
+	}
+	else
+	{
+		cout<<(a[0] + a[1] + a[2])/3;
+	}
+	return 0;
+}
